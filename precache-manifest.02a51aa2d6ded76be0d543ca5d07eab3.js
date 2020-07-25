a83c4f3f792084548f05b0d15883ed142cc68898
XunGong <enjoygrant@163.com>
2020-07-26 02:57:33 +0800

    Updates

---
+++ precache-manifest.02a51aa2d6ded76be0d543ca5d07eab3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "130ab61c5cb7f2258014ab4bcd23890e",
+    "url": "/simple-github-search/index.html"
+  },
+  {
+    "revision": "e0eeac65605316c59cc4",
+    "url": "/simple-github-search/static/css/2.fc5a9450.chunk.css"
+  },
+  {
+    "revision": "6ba54d58617ab84c94ba",
+    "url": "/simple-github-search/static/css/main.27800fa8.chunk.css"
+  },
+  {
+    "revision": "e0eeac65605316c59cc4",
+    "url": "/simple-github-search/static/js/2.5d2aac88.chunk.js"
+  },
+  {
+    "revision": "c0554f266784fabb0402ff1139c47fda",
+    "url": "/simple-github-search/static/js/2.5d2aac88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ba54d58617ab84c94ba",
+    "url": "/simple-github-search/static/js/main.6e819b84.chunk.js"
+  },
+  {
+    "revision": "701ff0699c7f0ffc954d",
+    "url": "/simple-github-search/static/js/runtime-main.67d0ab18.js"
+  }
+]);
